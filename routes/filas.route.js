984fb5b87bb26d6d303844376afc512693e2d254
Jalan <lucas.gregorio.lgs@gmail.com>
2025-05-12 15:29:59 -0300

    a

--- routes/filas.route.js
+++ routes/filas.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const login = require("../middleware/filas.middleware");8
+
+router.post("/", () => {console.log("Rota de filas")});
 
 
 
 
-
-
-
-module.exports
+module.exports = router;
